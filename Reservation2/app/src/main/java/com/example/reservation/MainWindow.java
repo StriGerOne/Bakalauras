f161127826fa4d5942b458bf3b5571901f89e973
StriGerOne <g.striukys@gmail.com>
2022-04-06 01:04:11 +0300

    Siek tiek spalvu

--- Reservation2/app/src/main/java/com/example/reservation/MainWindow.java
+++ Reservation2/app/src/main/java/com/example/reservation/MainWindow.java
@@ -2,20 +2,25 @@ package com.example.reservation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
-
+import com.squareup.picasso.Picasso;
 
 public class MainWindow extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ImageView login = findViewById(R.id.login_userboard);
+
+
+        Picasso.get()
+                .load("https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png")
+                .into(login);
 
         Intent currentIntent = getIntent();
         String userId = currentIntent.getStringExtra("UserInfo");
